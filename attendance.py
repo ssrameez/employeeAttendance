a37fa8c0037235d91ea01726386501037df5f4b2
Anurag Prajapat <anu42686@gmail.com>
2020-07-12 20:56:24 +0530

    Update attendance.py

--- attendance.py
+++ attendance.py
@@ -1,4 +1,5 @@
-from helperMehods import *
+from helperMethods import *
+employeeAttendance = None
 
 class EmpNode:
     def __init__(self, EId):
@@ -55,31 +56,55 @@ def _frequentVisitorRec(eNode, freq):
             print (str(emp.EmpId) + ", " + str(emp.attCtr))
     else:
         print("No employee swiped more than " +str(freq)+ " times today")
+    clearList()
 
 def _printRangePresent(eNode, StartId, EndId):
     findEmployeesInRange(eNode, StartId, EndId)
     if(len(listOfEmployees) > 0):
         print("Range "+str(StartId)+ " to " +str(EndId))
+        print("Employee swipe:")
         for emp in listOfEmployees:
             print (str(emp.EmpId) + ", " + str(emp.attCtr) + ", " + ("in" if emp.attCtr%2 != 0 else "out"))
     else:
         print("No employee in range "+str(StartId)+ " to " +str(EndId))
+    clearList()
+
+def employeeEntry():
+    global employeeAttendance
+    f=open("inputPS5.txt", "r") 
+    Lines = f.readlines() 
+    count = 1
+    for line in Lines: 
+        empId = int(line.strip())
+        if count == 1:
+            employeeAttendance = EmpNode(empId)
+        else:
+            _recordSwipeRec(employeeAttendance, empId)
+        count = count + 1
+
+def prompts():
+    
+    global employeeAttendance
+    
+    f=open("promptsPS5.txt", "r") 
+    Lines = f.readlines()
+    for line in Lines: 
+        # print(line)
+        if line.find("onPremises") != -1:
+            _getSwipeRec(employeeAttendance)
+            _onPremisesRec(employeeAttendance)
+        elif line.find("checkEmp") != -1:
+            empId = int(line.split(":")[1].strip())
+            _checkEmpRec(employeeAttendance, empId)
+        elif line.find("freqVisit") != -1:
+            freq = int(line.split(":")[1].strip())
+            _frequentVisitorRec(employeeAttendance, freq)
+        elif line.find("range") != -1:
+            startId = int(line.split(":")[1].strip())
+            endId = int(line.split(":")[2].strip())
+            _printRangePresent(employeeAttendance, startId , endId)
+            
 
 
-employeeAttendance = EmpNode(21)
-
-_recordSwipeRec(employeeAttendance, 23)
-_recordSwipeRec(employeeAttendance, 22)
-_recordSwipeRec(employeeAttendance, 41)
-_recordSwipeRec(employeeAttendance, 21)
-
-_getSwipeRec(employeeAttendance)
-
-_onPremisesRec(employeeAttendance)
-
-_checkEmpRec(employeeAttendance, 21)
-_checkEmpRec(employeeAttendance, 22)
-_checkEmpRec(employeeAttendance, 222)
-
-_frequentVisitorRec(employeeAttendance, 0)
-_printRangePresent(employeeAttendance, 21 , 22)
+employeeEntry()
+prompts()
